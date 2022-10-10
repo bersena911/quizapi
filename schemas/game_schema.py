16568b161b6fe9a1ca2f0286a05447b8ad2b3fc7
Mate Bersenadze <berse911@gmail.com>
2022-10-10 15:03:53 +0400

    add float roundings

--- schemas/game_schema.py
+++ schemas/game_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, UUID4
+from pydantic import BaseModel, UUID4, validator, root_validator
 
 
 class GameStartSchema(BaseModel):
@@ -18,12 +18,25 @@ class GameResponse(BaseModel):
 class QuestionStat(BaseModel):
     answer_score: float
     title: str
+    answer_score_percentage: float
+
+    @root_validator
+    def answer_score_percentage(cls, values):
+        values["answer_score_percentage"] = float(
+            "{:.3f}".format(values["answer_score"] * 100)
+        )
+        values["answer_score"] = float("{:.3f}".format(values["answer_score"]))
+        return values
 
 
 class FinalResultsResponse(BaseModel):
     score: float
     score_percentage: float
     question_stats: list[QuestionStat]
+
+    @validator("score", "score_percentage")
+    def round_float(cls, field_value):
+        return float("{:.3f}".format(field_value))
 
 
 class GameDetailResponse(BaseModel):
@@ -38,3 +51,7 @@ class QuizGamesResponse(BaseModel):
     user_id: UUID4
     title: str
     username: str
+
+    @validator("score")
+    def round_float(cls, score):
+        return float("{:.3f}".format(score))
